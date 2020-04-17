e8329ba33ed8718fbe5bcc49e1256412345da7e1
daaleksandrov <daaleksandrov@domclick.ru>
2020-04-18 01:06:09 +0300

    Refactoring

--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -11,30 +11,13 @@ import { Observable } from 'rxjs';
 export class ListingComponent implements OnInit {
   items$: Observable<any>;
   name: string;
-  url: string;
 
-  isActive = false;
   path = this.route.snapshot.url[0]?.path;
 
   constructor(private db: AngularFireDatabase, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.items$ = this.db.list(`/${ this.path }`).valueChanges();
-  }
-
-  onGetUrl({ url }) {
-    this.url = url;
-
-    if (this.name !== '') {
-      this.isActive = true;
-    }
-  }
-
-  onAdd() {
-    this.db.list(`/${ this.path }`).push({ name: this.name, img: this.url });
-
-    this.name = '';
-    this.url = '';
   }
 
   trackBy(index, country) {
