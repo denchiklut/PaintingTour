707eabe978f6166ce4e429ef7d9d57d1ff5e81fb
daaleksandrov <daaleksandrov@domclick.ru>
2020-04-23 10:14:44 +0300

    Small refactoring

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { increment, decrement, reset } from '../../../actions';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
