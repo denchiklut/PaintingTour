d9bc171adbc997ee1074d978e88c218e7bffd2a5
daaleksandrov <daaleksandrov@domclick.ru>
2020-05-08 22:51:37 +0300

    Add env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAwLkgpArKbmYBW7VnfeyTXg6ElTBXZmVw',
+    authDomain: 'paintingtour-dc7df.firebaseapp.com',
+    databaseURL: 'https://paintingtour-dc7df.firebaseio.com',
+    projectId: 'paintingtour-dc7df',
+    storageBucket: 'paintingtour-dc7df.appspot.com',
+    messagingSenderId: '348291594619',
+    appId: '1:348291594619:web:d6629e60210bd3c699e5de',
+    measurementId: 'G-YZDBXN0D12'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAwLkgpArKbmYBW7VnfeyTXg6ElTBXZmVw',
+    authDomain: 'paintingtour-dc7df.firebaseapp.com',
+    databaseURL: 'https://paintingtour-dc7df.firebaseio.com',
+    projectId: 'paintingtour-dc7df',
+    storageBucket: 'paintingtour-dc7df.appspot.com',
+    messagingSenderId: '348291594619',
+    appId: '1:348291594619:web:d6629e60210bd3c699e5de',
+    measurementId: 'G-YZDBXN0D12'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
